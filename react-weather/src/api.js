959addd544e859e8a2cfa846bafac9ac33178c27
dikshant4yu <dikshantclownprince@gmail.com>
2024-04-09 19:30:12 +0530

    Last Push

---
+++ react-weather/src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dc41699c3dmsh5ad70158768f1fap1a9531jsn204a8f1bd57c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "222045b33de66befad20a700868abce9";
